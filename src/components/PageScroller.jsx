97c50f10d8870010be3f033bbfc6dd95c1c549c8
orekode <shalommckdavid@gmail.com>
2024-02-10 11:05:45 +0000

    commit before pull

--- src/components/PageScroller.jsx
+++ src/components/PageScroller.jsx
@@ -17,12 +17,12 @@ const PageScroller = () => {
     return () => { window.removeEventListener('scroll', handleScroll) };
   }, []);
 
-  console.log(position, window.scroll);
+  console.log(position, window.scrollHeight);
 
 
-  if(position > window.scrollHeight / 2)
+  if(position > document.querySelector("body").scrollHeight / 2)
   return (
-    <div className="fixed bottom-5 right-10 z-50 flex-col flex-center max-[700px]:hidden">
+    <div onClick={() => window.scrollTo(0, 0)} className="fixed bottom-5 right-10 z-50 flex-col flex-center max-[700px]:hidden">
         <span className="text-nowrap w-max">Back Top</span>
         <div className="rounded-3xl h-[45px] w-[45px] border-2 border-blue-600 border-dashed relative  animate-bg">
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-45 dark:hidden w-max h-max animate-obj">
